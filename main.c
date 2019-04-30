009432b70fc63622c54a18146ce3252fa99edcb4
Shyam <shyam.merchant99@gmail.com.com>
2019-04-30 12:41:33 -0400

    Relatively new UI

--- main.c
+++ main.c
@@ -31,7 +31,7 @@ typedef struct User {
         User;
 
 int main() {
-    int presentValue = -1;
+    int presentValue = 0;
     int userId=-1;
     int optionOne = 0;
     int option = 0;
@@ -46,6 +46,12 @@ int main() {
         arr_Users[i].passwd = NULL;
         arr_Users[i].uid = NULL;
     }
+    arr_Users[0].first_name ="Shyam";
+    arr_Users[0].last_name = "Merchant";
+    arr_Users[0].username = "smercha";
+    arr_Users[0].passwd = "abcd@123";
+    arr_Users[0].uid = 0;
+    /*
     for (int i = 0; i < 50; i++) {
         if (arr_Users[i].uid == i) {
             presentValue++;
@@ -53,6 +59,7 @@ int main() {
             break;
         }
     }
+     */
     displayWelcome();
     do {
         printf("Press 1. to Login: \n");
@@ -61,15 +68,17 @@ int main() {
         scanf("%d", &optionOne);
 
         if (optionOne == 1) {
-            char *loginUsername = NULL;
+            char *loginUsername;
             loginUsername = malloc(sizeof(char *));
-            char *loginPassword = NULL;
+            char *loginPassword;
             loginPassword == malloc(sizeof(char *));
             printf("Please enter your username: \n");
             scanf("%s", loginUsername);
             printf("Please enter your password: \n");
             scanf("%s", loginPassword);
-            for (int i = 0; i < presentValue; i++)
+            loginFunctions(arr_Users[0].uid,arr_Users);
+            /*
+            for (int i = 0; i < 50; i++)
             {
                 if (arr_Users[i].username==loginUsername && arr_Users[i].passwd==loginPassword)
                 {
@@ -85,6 +94,7 @@ int main() {
             else if (grantAccess == 0) {
                 printf("The Login was incorrect: \n");
             }
+             */
         }
         if (optionOne == 2) {
             char *First_Name = NULL;
@@ -98,7 +108,8 @@ int main() {
             printf("Enter Details for User %d \n", presentValue + 1);
             printf("Present Value: %d", presentValue);
 
-            for (int i = presentValue; i < presentValue + 1; i++) {
+            for (int i = presentValue; i < presentValue + 1; i++)
+            {
                 printf("Please Enter the First Name:");
                 scanf("%s", First_Name);
                 printf("Please Enter the Last Name:");
@@ -113,8 +124,9 @@ int main() {
                 arr_Users[i].username = uname;
                 arr_Users[i].passwd = pass;
             }
-            //presentValue++;
-            for (int i = presentValue; i < presentValue + 1; i++) {
+
+            for (int i = presentValue; i < presentValue + 1; i++)
+            {
                 printf("***************************************** \n");
                 printf("User #%d \n", arr_Users[i].uid + 1);
                 printf("First Name is %s \n", arr_Users[i].first_name);
@@ -123,10 +135,11 @@ int main() {
                 printf("Password is %s \n", arr_Users[i].passwd);
             }
             printf("***************************************** \n");
+            presentValue++;
         }
 
         if (optionOne == 3) {
-
+        loginFunctions(arr_Users[0].uid,arr_Users);
         }
     }
     while(optionOne!=-1);
@@ -148,9 +161,9 @@ IndividualAccount *CreateNewAccount(char *username, char *passwd) {
 void displayWelcome() {
     printf("                      ███████                       \n");
     printf("                      ███11███                     \n");
-    printf("                      ██1111██                      \n");
+    printf("                      ██11111██                      \n");
     printf("                      ███11███                      \n");
-    printf("                       ██11██                       \n");
+    printf("                       ██111██                       \n");
     printf("                      ██████                       \n");
     printf("                     ████████                      \n");
     printf("                  ████████████                    \n");
@@ -195,32 +208,165 @@ void displayWelcome() {
 }
 void loginFunctions(int presentValue,User arr_Users[50])
 {
+    int encrypt();
+    int currValue=presentValue;
     int option=0;
+    struct IndividualAccount arr_Accounts[50];
     do {
 
 
         printf("Press 1. To add an account to your user \n");
-        printf("Press 4. To Login to your account: \n");
+        printf("Press 2. To view the accounts saved in the username \n");
+        printf("Press 3. Check Password Strength: \n");
+        printf("Press 4. To Encrypt Passwords \n");
         printf("Press -1. To end the program: \n");
         scanf("%d", &option);
-        if (option == 1) {
-            if (arr_Users[0].uid = NULL) {
+        if (option == 1)
+        {
+
+            if (arr_Users[currValue].uid = NULL) {
                 printf("You do not have an account! Can not add details. \n");
-            } else {
-                char *user_Name = NULL;
-                user_Name = malloc(sizeof(char *));
-                char *password = NULL;
-                password = malloc(sizeof(char *));
-                printf("Please enter the Username/ Email which you want to add: \n");
-                scanf("%s", user_Name);
-                printf("Please enter the password of the account: \n");
-                scanf("%s", password);
-                arr_Users[0].ac.userName = user_Name;
-                arr_Users[0].ac.password = password;
-                printf("Username is: %s \n", arr_Users[0].ac.userName);
-                printf("Password is: %s \n", arr_Users[0].ac.password);
+            } else
+                {
+                for (int i=currValue;i<currValue+1;i++)
+                {
+                    char *user_Name = NULL;
+                    user_Name = malloc(sizeof(char *));
+                    char *password = NULL;
+                    password = malloc(sizeof(char *));
+                    printf("Please enter the Username/ Email which you want to add: \n");
+                    scanf("%s", user_Name);
+                    printf("Please enter the password of the account: \n");
+                    scanf("%s", password);
+                    arr_Accounts[currValue].aid=currValue;
+                    arr_Accounts[currValue].userName = user_Name;
+                    arr_Accounts[currValue].password=password;
+                    printf("Username is: %s \n", arr_Accounts[currValue].userName);
+                    printf("Password is: %s \n", arr_Accounts[currValue].password);
+                }
+                currValue++;
             }
         }
+        if(option==2)
+        {
+            for (int i = presentValue; i < presentValue+1; i++)
+            {
+                printf("***************************************** \n");
+                printf("User #%d \n", arr_Users[i].uid + 1);
+                printf("First Name is %s \n", arr_Users[i].first_name);
+                printf("Last Name is %s  \n", arr_Users[i].last_name);
+                printf("Username is %s  \n", arr_Users[i].username);
+                printf("Password is %s \n", arr_Users[i].passwd);
+
+            }
+            printf("***************************************** \n");
+            printf("***************************************** \n");
+            printf("***************************************** \n");
+            for(int i=presentValue;i<currValue;i++)
+            {
+                printf("The %d account saved for user is: \n", arr_Accounts[i].aid);
+                printf("Username: %s\n", arr_Accounts[i].userName);
+                printf("Password: %s\n", arr_Accounts[i].password);
+            }
+        }
+        if(option==3)
+        {
+            void check(char a[]);
+            char a[100];
+            printf("Enter your password that should satisfy the following criteria\n1. password should contain atleast one capital letter\n2. should have one digit from(o-9)\n3. atleast one special charecter($,*,#,&)\n4. length should be atleast 6\n");
+            scanf("%s",a);
+            check(a);
+        }
+        if(option==4)
+        {
+         encrypt();
+        }
+
 
     } while (option != -1);
+}
+void check(char a[])
+{
+    char c;
+    int len,i,flag1=0,flag2=0,flag3=0,flag4=0;
+    len=strlen(a);
+    if(len<6)
+        flag1=1;
+    else
+    {
+        for(i=0;i<len;i++)
+            if((a[i]>=48&&a[i]<=58))
+            {
+                flag2=0;
+
+                break;
+            }
+            else
+                flag2=1;
+
+        for(i=0;i<len;i++)
+            if((a[i]>=65&&a[i]<=90))
+            {
+                flag3=0;
+
+                break;
+            }
+            else
+                flag3=1;
+
+        for(i=0;i<len;i++)
+            if(a[i]=='#'||a[i]=='$'||a[i]=='*'||a[i]=='&')
+            {
+                flag4=0;
+
+                break;
+            }
+            else
+                flag4=1;
+
+
+    }
+    if(flag1==1||flag2==1||flag3==1||flag4==1)
+        printf("\nPassword Standards Not Met: \n");
+    else
+        printf("your password is successfully created\n");
+
+
+    return;
+}
+int encrypt()
+{
+    int i, x;
+    char str[100];
+
+    printf("\nPlease enter a string:\t");
+    gets(str);
+
+    printf("\nPlease choose following options:\n");
+    printf("1 = Encrypt the string.\n");
+    printf("2 = Decrypt the string.\n");
+    scanf("%d", &x);
+
+    //using switch case statements
+    switch(x)
+    {
+        case 1:
+            for(i = 0; (i < 100 && str[i] != '\0'); i++)
+                str[i] = str[i] + 3; //the key for encryption is 3 that is added to ASCII value
+
+            printf("\nEncrypted string: %s\n", str);
+            break;
+
+        case 2:
+            for(i = 0; (i < 100 && str[i] != '\0'); i++)
+                str[i] = str[i] - 3; //the key for encryption is 3 that is subtracted to ASCII value
+
+            printf("\nDecrypted string: %s\n", str);
+            break;
+
+        default:
+            printf("\nError\n");
+    }
+    return 0;
+
 }
